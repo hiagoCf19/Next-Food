00e78744a52ee58435445937aa80d40505a77ba2
hiagoCf19 <hiagoferreira@id.uff.br>
2024-05-03 11:17:15 -0300

    feat: add initial header component

--- app/about/page.tsx
+++
@@ -1,9 +0,0 @@
-
-
-import { useState } from "react"
-
-const About = () => {
-
-  return <h1>About page</h1>
-}
-export default About
--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <h1>page</h1>
+
         {children}
       </body>
     </html>
