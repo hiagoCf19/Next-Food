3af83f60ecf5327d5861638f76c9576882160054
hiagoCf19 <hiagoferreira@id.uff.br>
2024-05-03 14:48:01 -0300

    refactor:  passando produtos para o componente de lista via props

--- app/components/products-list.tsx
+++ app/components/products-list.tsx
@@ -1,14 +1,9 @@
-import { db } from "../lib/prisma";
+
+import { Prisma } from "@prisma/client";
 import ProductItem from "./products-item";
 
-const ProductList = async () => {
-  const products = await db.product.findMany({
-    where: {
-      discountPercentage: {
-        gt: 0,
-      }
-    },
-    take: 10,
+interface ProductListProps {
+  products: Prisma.ProductGetPayload<{
     include: {
       restaurant: {
         select: {
@@ -16,7 +11,10 @@ const ProductList = async () => {
         }
       }
     }
-  })
+  }>[]
+}
+const ProductList = async ({ products }: ProductListProps) => {
+
 
   return (
     <div className="flex overflow-x-scroll [&::-webkit-scrollbar]:hidden gap-4 px-5 ">
--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,24 @@ import Search from "./components/search";
 import ProductList from "./components/products-list";
 import { Button } from "./components/ui/button";
 import { ChevronRightIcon } from "lucide-react";
+import { db } from "./lib/prisma";
 
-export default function Home() {
+const Home = async () => {
+  const products = await db.product.findMany({
+    where: {
+      discountPercentage: {
+        gt: 0,
+      }
+    },
+    take: 10,
+    include: {
+      restaurant: {
+        select: {
+          name: true,
+        }
+      }
+    }
+  })
   return (
     <>
       <Header />
@@ -38,10 +54,11 @@ export default function Home() {
             <ChevronRightIcon size={16} />
           </Button>
         </div>
-        <ProductList />
+        <ProductList products={products} />
       </div>
 
     </>
 
   );
 }
+export default Home
