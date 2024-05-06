46306f3e2cccde59cf08ad076bb0bc1bd477d562
hiagoCf19 <hiagoferreira@id.uff.br>
2024-05-06 15:04:38 -0300

    feat: navegação das recomendados à home

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,12 +1,23 @@
 import Image from "next/image";
 import { Button } from "./ui/button";
 import { MenuIcon } from 'lucide-react';
+import Link from "next/link";
 
 
 const Header = () => {
   return (
     <div className="flex justify-between pt-6 px-5 ">
-      <Image src={"/logo.png"} alt="ifood" width={80} height={30} />
+      <div className="relative h-9 w-20">
+        <Link href={"/"}>
+          <Image
+            src={"/logo.png"}
+            alt="ifood"
+            fill
+            className="object-contain"
+          />
+        </Link>
+
+      </div>
       <Button
         size="icon"
         variant="outline"
