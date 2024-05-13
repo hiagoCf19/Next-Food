45b71c49edaed06ebec3edc129f580759c7ae504
hiagoCf19 <hiagoferreira@id.uff.br>
2024-05-13 12:26:03 -0300

    feat: add alert in cart

--- app/components/cart.tsx
+++ app/components/cart.tsx
@@ -9,9 +9,11 @@ import { createOrder } from "../_actions/order";
 import { OrderStatus } from "@prisma/client";
 import { useSession } from "next-auth/react";
 import { Loader2 } from "lucide-react";
+import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from "./ui/alert-dialog";
 
 const Cart = () => {
   const [isSubmitLoading, setIsSubmitLoading] = useState(false)
+  const [isConfirmDialogOpen, setIsConfirmDialogOpen] = useState(false)
 
   const { data } = useSession();
   const { products,
@@ -52,68 +54,83 @@ const Cart = () => {
   }
 
   return (
+    <>
+      <div className="space-y-2 py-3 relative overflow-y-scroll max-h-[60%] [&::-webkit-scrollbar]:hidden">
+        {products.map((product) => (
+          <CartItem cartProduct={product} key={product.id} />
+        ))}
 
-    <div className="space-y-2 py-3 relative overflow-y-scroll max-h-[60%] [&::-webkit-scrollbar]:hidden">
-      {products.map((product) => (
-        <CartItem cartProduct={product} key={product.id} />
-      ))}
+        {products.length > 0 ? (
+          <div className="fixed bottom-0 pb-4 w-[80%]">
 
-      {products.length > 0 ? (
-        <div className="fixed bottom-0 pb-4 w-[80%]">
+            <Card className="border-none shadow-none ">
+              <CardContent className="p-4 space-y-4">
+                <div className="flex justify-between items-center text-xs">
+                  <span className="text-muted-foreground">Subtotal </span>
+                  <span>{formatCurrency(subtotalPrice)}</span>
+                </div>
+                <Separator className="bg-[#a7a7a766]" />
+                <div className="flex justify-between items-center text-xs">
+                  <span className="text-muted-foreground">Descontos</span>
+                  <span> - {formatCurrency(totalDiscounts)}</span>
+                </div>
+                <Separator className="bg-[#a7a7a766]" />
+                <div className="flex justify-between items-center text-xs">
+                  <span className="text-muted-foreground">Entrega </span>
+                  <span>
+                    {Number(products?.[0].restaurant.deliveryFee) === 0
+                      ? <span className="uppercase text-[#74cb14] font-semibold">Grátis</span>
+                      : formatCurrency(Number(products?.[0].restaurant.deliveryFee))}
+                  </span>
+                </div>
 
-          <Card className="border-none shadow-none ">
-            <CardContent className="p-4 space-y-4">
-              <div className="flex justify-between items-center text-xs">
-                <span className="text-muted-foreground">Subtotal </span>
-                <span>{formatCurrency(subtotalPrice)}</span>
-              </div>
-              <Separator className="bg-[#a7a7a766]" />
-              <div className="flex justify-between items-center text-xs">
-                <span className="text-muted-foreground">Descontos</span>
-                <span> - {formatCurrency(totalDiscounts)}</span>
-              </div>
-              <Separator className="bg-[#a7a7a766]" />
-              <div className="flex justify-between items-center text-xs">
-                <span className="text-muted-foreground">Entrega </span>
-                <span>
-                  {Number(products?.[0].restaurant.deliveryFee) === 0
-                    ? <span className="uppercase text-[#74cb14] font-semibold">Grátis</span>
-                    : formatCurrency(Number(products?.[0].restaurant.deliveryFee))}
-                </span>
-              </div>
+                <Separator className="bg-[#a7a7a766]" />
 
-              <Separator className="bg-[#a7a7a766]" />
-
-              <div className="flex justify-between items-center text-xs font-semibold">
-                <span>Total</span>
-                <span> {formatCurrency(totalPrice)}</span>
-              </div>
-
-
+                <div className="flex justify-between items-center text-xs font-semibold">
+                  <span>Total</span>
+                  <span> {formatCurrency(totalPrice)}</span>
+                </div>
 
 
-            </CardContent>
-          </Card>
-
-          <Button
-            className="w-full mt-2"
-            onClick={handleFinishOrderClick}
-            disabled={isSubmitLoading}
 
 
-          >
-            {isSubmitLoading && (<Loader2 className="mr-2 h-4 w-4 animate-spin" />)}
-            Finalizar pedido
-          </Button>
-        </div>
-      ) :
-        <h2 className=" text-muted-foreground text-sm h-[90vh]  flex items-center justify-center">
-          Sua sacola está vazia
-        </h2>
-      }
+              </CardContent>
+            </Card>
+
+            <Button
+              className="w-full mt-2"
+              onClick={() => setIsConfirmDialogOpen(true)}
+              disabled={isSubmitLoading}
+            >
+              {isSubmitLoading && (<Loader2 className="mr-2 h-4 w-4 animate-spin" />)}
+              Finalizar pedido
+            </Button>
+          </div>
+        ) :
+          <h2 className=" text-muted-foreground text-sm h-[90vh]  flex items-center justify-center">
+            Sua sacola está vazia
+          </h2>
+        }
 
 
-    </div>
+      </div>
+
+      <AlertDialog open={isConfirmDialogOpen} onOpenChange={setIsConfirmDialogOpen} >
+
+        <AlertDialogContent className="w-[90%]">
+          <AlertDialogHeader>
+            <AlertDialogTitle>Deseja concluir seu pedido?</AlertDialogTitle>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel >
+              Cancelar
+            </AlertDialogCancel>
+            <AlertDialogAction onClick={handleFinishOrderClick}>Concluir</AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    </>
+
 
   );
 }
